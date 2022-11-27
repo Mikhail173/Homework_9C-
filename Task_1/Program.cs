23b6dce8954f2640ff25a1b8119885ce4c995258
Mikhail173 <2016.Nevermind.2020@gmail.com>
2022-11-27 17:28:40 +0300

    Task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Input natural number: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+
+int Natural (int n, int a)
+{
+    if (n == a)
+    {
+        return n;
+    }
+    else
+    {
+    Console.Write(Natural(n, a+1) + ", ");
+    }
+    return a;
+}
+Console.WriteLine(Natural(n, a));
